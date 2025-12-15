643e624f526baf29436fbad5ba215e451b9ab02d
sarthak bansal <sarthakb@adobe.com>
2025-12-15 11:22:05 +0530

    bootstarp cdn

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// Bootstrap is lazy loaded in index.html
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./index.css";
 // import './components/assets/js/app.min'
 // import './components/assets/js/vendor.min'
 // import './components/assets/js/vendor/apexcharts.min'
 // import './components/assets/js/vendor/jquery-jvectormap-1.2.2.min'
 // import './components/assets/js/pages/demo.dashboard'
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
